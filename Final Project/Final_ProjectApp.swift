45f2306111b1e8d4cb08ab553420259b8b900a0c
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2023-12-05 09:37:09 -0600

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Fabiola Rocha Marquez on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
