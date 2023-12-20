cfb49636e1617b4ace204b325881482947c42427
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2023-12-20 07:46:18 -0600

    finished project

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -31,10 +31,6 @@ struct InstructionsView: View {
     var body: some View {
         ZStack{
             Color.blue.opacity(0.4).ignoresSafeArea()
-        VStack {
-            Button("Start") {
-               
-            }
             RoundedRectangle(cornerRadius: 30)
                 .frame(width: 100, height: 100)
                 .foregroundStyle(Color.purple.opacity(0.7))
@@ -44,43 +40,42 @@ struct InstructionsView: View {
             RoundedRectangle(cornerRadius: 30)
                 .frame(width: 100, height: 100)
                 .foregroundStyle(Color.purple.opacity(0.7))
-                .offset(x: isCenter ? 0 : 50)
-                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
-          
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 100, height: 100)
-                .foregroundStyle(Color.purple.opacity(0.7))
-                .offset(x: isCenter ? 0 : 50)
-                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
-           
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 100, height: 100)
-                .foregroundStyle(Color.purple.opacity(0.7))
-                .offset(x: isCenter ? 0 : 50)
-                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
-          
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 100, height: 100)
-                .foregroundStyle(Color.purple.opacity(0.7))
-                .offset(x: isCenter ? 0 : 50)
-                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
-           
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 100, height: 100)
-                .foregroundStyle(Color.purple.opacity(0.7))
-                .offset(x: isCenter ? 0 : 50)
+                .offset(x: isCenter ? 0 : -50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
             
-            }
-        .onAppear() {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 100, height: 100)
+                .foregroundStyle(Color.purple.opacity(0.7))
+                .offset(y: isCenter ? 0 : 50)
+                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
             
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 100, height: 100)
+                .foregroundStyle(Color.purple.opacity(0.7))
+                .offset(y: isCenter ? 0 : -50)
+                .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+            
+            Text("Relax")
+                .fontWeight(.bold)
         }
+        .onAppear() {
+            let animation = Animation.linear(duration: 4)
+            withAnimation(animation.repeatForever(autoreverses: true)) {
+                self.isCenter = false
+                }
+            withAnimation(animation.repeatForever(autoreverses: true)) {
+                self.isCenter2 = false
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+// https://www.youtube.com/watch?v=wVvJMIdT2Ko
+
